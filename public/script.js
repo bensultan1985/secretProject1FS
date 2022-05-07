f6674a56910ed0a8b82b6041932781795ad28518
bensultan1985 <bensultan1985@gmail.com>
2022-05-06 18:29:18 -0700

    test change

--- public/script.js
+++ public/script.js
@@ -123,7 +123,7 @@ gameData = {
     },
     "04/27/2022": {
         "name": ["DAVID LETTERMAN", "DAVE LETTERMAN", "DAVE", "LETTERMAN"],
-        "image": "DAVID_LETTERMAN.jpg",
+        "image": "DAVID_LETTERMAN2.jpg",
         "issue": "24"
     }
 
